5d0cd1d56299843b1f7d987a3a7139b11fb7a0a2
annisadiann <annisadian0412@gmail.com>
2026-01-02 21:42:29 +0700

    memperbaiki PenyediaViewModel.kt dan PetaNavigasi.kt

--- app/src/main/java/com/example/praktikum9/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/praktikum9/view/controllNavigasi/PetaNavigasi.kt
@@ -1,20 +1,22 @@
 package com.example.praktikum9.view.controllNavigasi
 
-import HomeScreen
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.praktikum9.view.EntrySiswaScreen
+import com.example.praktikum9.view.HomeScreen
+import com.example.praktikum9.view.route.DestinasiDetail
+import com.example.praktikum9.view.route.DestinasiEntry
+import com.example.praktikum9.view.route.DestinasiHome
 
 @Composable
-fun DataSiswaApp(
-    navController: NavController = rememberNavController(),
-    modifier: Modifier
-) {
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier){
     HostNavigasi(navController = navController)
 }
 
@@ -22,24 +24,18 @@ fun DataSiswaApp(
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-) {
-    NavHost(
-        navController = navController,
-        startDestination = DestinasiHome.route,
-        modifier = Modifier
-    ) {
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier) {
         composable(DestinasiHome.route) {
-            HomeScreen(
-                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route)},
                 navigateToItemUpdate = {
-                    navController.navigate("${DestinasiDetail.route}/${it}")
-                }
-            )
+                    navController.navigate(("${DestinasiDetail.route}/${it}"))
+                })
         }
-        composable(DestinasiEntry.route) {
-            EntrySiswaScreen(
-                navigateBack = { navController.navigate(DestinasiHome.route)}
-            )
+        composable (DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.navigate(DestinasiHome
+                .route)})
         }
     }
 }
--- app/src/main/java/com/example/praktikum9/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum9/viewmodel/PenyediaViewModel.kt
@@ -1,17 +1,20 @@
 package com.example.praktikum9.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import androidx.lifecycle.ViewModelProvider
+import com.example.praktikum9.repositori.AplikasiDataSiswa
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
-        AplikasiDataSiswa
+                AplikasiDataSiswa
         )
+
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer {HomeViewModel(aplikasiDataSiswa().container.repositorySiswa)}
-        initializer {EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)}
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
     }
 }
