27e4c263ff83e8981c7c34a65e8873f0ba9fba36
annisadiann <annisadian0412@gmail.com>
2026-01-02 21:25:58 +0700

    add file DestinasiEdit.kt

---
+++ app/src/main/java/com/example/praktikum9/view/route/DestinasiEdit.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum9.view.route
+
+import com.example.praktikum9.R
+
+object DestinasiEdit : DestinasiNavigasi{
+    override val route = "item_edit"
+    override val titleRes =  R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
