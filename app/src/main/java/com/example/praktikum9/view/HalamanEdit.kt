70ca5153600a6675e93ddb39d9e233341b847ac3
annisadiann <annisadian0412@gmail.com>
2026-01-08 19:48:29 +0700

    Menghubungkan ViewModel dengan form edit dan update

--- app/src/main/java/com/example/praktikum9/view/HalamanEdit.kt
+++ app/src/main/java/com/example/praktikum9/view/HalamanEdit.kt
@@ -31,3 +31,7 @@ fun EditSiswaScreen(
         },
         modifier = modifier
     ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
